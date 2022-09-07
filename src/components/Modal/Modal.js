24e33a411718c37a049b237f7b5d77728d9441fe
LeonidGarasko <djjiehuh@gmail.com>
2022-09-07 20:08:05 +0300

    upd

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -21,7 +21,7 @@ const Modal = ({ onClose, src }) => {
       window.removeEventListener('keydown', handleKeyDown);
       window.removeEventListener('click', handleBackdropClick);
     };
-  }, []);
+  });
 
   return (
     <OverlayStyled onClick={handleBackdropClick}>
